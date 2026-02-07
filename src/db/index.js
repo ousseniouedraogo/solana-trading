1ce57f469e9e5fdb4f8b01c027772f366b16c711
bonbusiness95-cyber <bon.business95@gmail.com>
2026-02-07 23:49:42 +0000

    Fix: Force IPv4 for MongoDB connection

--- src/db/index.js
+++ src/db/index.js
@@ -14,6 +14,7 @@ const connectDB = async () => {
       maxPoolSize: 10,          // Maintain up to 10 socket connections
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000,   // Close sockets after 45 seconds of inactivity
+      family: 4 // Use IPv4, skip IPv6
     });
 
     // Set default query options for speed
